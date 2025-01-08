e21689f312870d512737a2d8689ebe74e3d4818f
Auke van Slooten <auke@muze.nl>
2025-01-08 15:24:14 +0100

    added keysstore to oidc global

--- src/browser.mjs
+++ src/browser.mjs
@@ -2,13 +2,17 @@ import * as oauth2module from './oauth2.mjs'
 import * as oauth2mockserver from './oauth2.mockserver.mjs'
 import * as oauth2discovery from './oauth2.discovery.mjs'
 import { tokenStore } from './tokenstore.mjs'
+import keysStore from './keysstore.mjs'
 import dpopmw from './oauth2.dpop.mjs'
 
 const oauth2 = Object.assign(oauth2module.default, oauth2module, {
 	mockserver: oauth2mockserver,
 	discovery: oauth2discovery,
 	tokenstore: tokenStore,
-	dpopmw
+	dpopmw,
+	keysstore: keysStore
 })
+
+globalThis.oauth2 = oauth2
 
 export default oauth2
