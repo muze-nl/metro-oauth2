c7e220649ca7e160b1a8a9c3a89416f28d4b8033
Auke van Slooten <auke@muze.nl>
2025-01-14 10:17:32 +0100

    tidied up, added oauth2 object to globalThis.metro

--- src/browser.mjs
+++ src/browser.mjs
@@ -1,19 +1,22 @@
 import metro from '@muze-nl/metro'
-import * as oauth2module from './oauth2.mjs'
+import oauth2mw, * as oauth2module from './oauth2.mjs'
 import * as oauth2mockserver from './oauth2.mockserver.mjs'
-import * as oauth2discovery from './oauth2.discovery.mjs'
+import * as oauth2discover from './oauth2.discovery.mjs'
 import { tokenStore } from './tokenstore.mjs'
 import keysStore from './keysstore.mjs'
 import dpopmw from './oauth2.dpop.mjs'
 
-const oauth2 = Object.assign(oauth2module.default, oauth2module, {
+const oauth2 = Object.assign(oauth2module, {
+	oauth2mw,
 	mockserver: oauth2mockserver,
-	discovery: oauth2discovery,
+	discover: oauth2discover,
 	tokenstore: tokenStore,
 	dpopmw,
 	keysstore: keysStore
 })
 
-metro.oauth2 = oauth2
+if (!globalThis.metro.oauth2) {
+	globalThis.metro.oauth2 = oauth2
+}
 
 export default oauth2
