3eaa4ad2f7a7f2bcc55bee9ae1d8cb9c6b75cd4e
Ben Peachey <ben@muze.nl>
2026-04-15 16:08:43 +0200

    Add test for handleRedirect() function.

---
+++ test/oauth2.popup.mjs
@@ -0,0 +1,137 @@
+import tap from 'tap'
+import { handleRedirect } from '../src/oauth2.popup.mjs'
+
+tap.test('handleRedirect SHOULD complain WHEN called without Window existing', async t => {
+    globalThis.window = undefined
+    t.throws(() => {
+        handleRedirect()
+    })
+    t.end()
+})
+
+tap.test('handleRedirect SHOULD complain WHEN called without parent existing', async t => {
+    globalThis.window = { location: { origin: 'mock origin' } }
+
+    const logs = t.capture(console, 'error')
+    const success = handleRedirect()
+
+    t.match(logs(), [
+        { args: ['No parent window found, cannot post authorization code (or error)'], returned: undefined },
+    ])
+    t.notOk(success)
+    t.end()
+})
+
+tap.test('handleRedirect SHOULD post Error message WHEN called with parent without query params', async t => {
+    globalThis.window = { location: { origin: 'mock origin' } }
+
+    globalThis.window.parent = {
+        postMessage: (message, origin) => {
+            t.equal(origin, 'mock origin')
+            t.match(message, { error: 'Could not find an authorization_code' })
+        }
+    }
+
+    const success = handleRedirect()
+
+    t.notOk(success)
+    t.end()
+})
+
+tap.test('handleRedirect SHOULD post Error message WHEN called with window opener', async t => {
+    globalThis.window = { location: { origin: 'mock origin' } }
+    globalThis.window.parent = globalThis.window
+
+    globalThis.window.opener = {
+        postMessage: (message, origin) => {
+            t.equal(origin, 'mock origin')
+            t.match(message, { error: 'Could not find an authorization_code' })
+        }
+    }
+
+    const success = handleRedirect()
+
+    t.notOk(success)
+    t.end()
+})
+
+tap.test('handleRedirect SHOULD post received error WHEN called with parent with error query param', async t => {
+    globalThis.window = { location: { origin: 'mock origin', search: '?error=mockError' } }
+
+    globalThis.window.parent = {
+        postMessage: (message, origin) => {
+            t.equal(origin, 'mock origin')
+            t.match(message, { error: 'mockError' })
+        }
+    }
+
+    const success = handleRedirect()
+
+    t.notOk(success)
+    t.end()
+})
+
+tap.test('handleRedirect SHOULD post received error WHEN called with parent with error hash param', async t => {
+    globalThis.window = { location: { origin: 'mock origin', search: '', hash: '#error=mockError' } }
+
+    globalThis.window.parent = {
+        postMessage: (message, origin) => {
+            t.equal(origin, 'mock origin')
+            t.match(message, { error: 'mockError' })
+        }
+    }
+
+    const success = handleRedirect()
+
+    t.notOk(success)
+    t.end()
+})
+
+tap.test('handleRedirect SHOULD post received code WHEN called with parent with code query param', async t => {
+    globalThis.window = { location: { origin: 'mock origin', search: '?code=mockCode' } }
+
+    globalThis.window.parent = {
+        postMessage: (message, origin) => {
+            t.equal(origin, 'mock origin')
+            t.match(message, { authorization_code: 'mockCode' })
+        }
+    }
+
+    const success = handleRedirect()
+
+    t.ok(success)
+    t.end()
+})
+
+tap.test('handleRedirect SHOULD post received code WHEN called with opener with code query param', async t => {
+    globalThis.window = { location: { origin: 'mock origin', search: '?code=mockCode' } }
+    globalThis.window.parent = globalThis.window
+
+    globalThis.window.opener = {
+        postMessage: (message, origin) => {
+            t.equal(origin, 'mock origin')
+            t.match(message, { authorization_code: 'mockCode' })
+        }
+    }
+
+    const success = handleRedirect()
+
+    t.ok(success)
+    t.end()
+})
+
+tap.test('handleRedirect SHOULD post received code WHEN called with parent with code hash param', async t => {
+    globalThis.window = { location: { origin: 'mock origin', search: '', hash: '#code=mockCode' } }
+
+    globalThis.window.parent = {
+        postMessage: (message, origin) => {
+            t.equal(origin, 'mock origin')
+            t.match(message, { authorization_code: 'mockCode' })
+        }
+    }
+
+    const success = handleRedirect()
+
+    t.ok(success)
+    t.end()
+})
