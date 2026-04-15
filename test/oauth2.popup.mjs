7aa7780d3562d8e0dc15402c6e62fc162df8c543
Ben Peachey <ben@muze.nl>
2026-04-15 17:02:50 +0200

    Change handleRedirect tests be more consistent and reduce duplication.

--- test/oauth2.popup.mjs
+++ test/oauth2.popup.mjs
@@ -3,9 +3,10 @@ import { handleRedirect } from '../src/oauth2.popup.mjs'
 
 tap.test('handleRedirect SHOULD complain WHEN called without Window existing', async t => {
     globalThis.window = undefined
-    t.throws(() => {
-        handleRedirect()
-    })
+
+    const actual = t.throws(handleRedirect)
+
+    t.equal(actual.message, "Cannot read properties of undefined (reading 'location')")
     t.end()
 })
 
@@ -13,125 +14,104 @@ tap.test('handleRedirect SHOULD complain WHEN called without parent existing', a
     globalThis.window = { location: { origin: 'mock origin' } }
 
     const logs = t.capture(console, 'error')
-    const success = handleRedirect()
+    const actual = handleRedirect()
 
     t.match(logs(), [
-        { args: ['No parent window found, cannot post authorization code (or error)'], returned: undefined },
+        { args: [ 'No parent window found, cannot post authorization code (or error)' ], returned: undefined },
     ])
-    t.notOk(success)
+    t.notOk(actual)
     t.end()
 })
 
 tap.test('handleRedirect SHOULD post Error message WHEN called with parent without query params', async t => {
     globalThis.window = { location: { origin: 'mock origin' } }
-
     globalThis.window.parent = {
-        postMessage: (message, origin) => {
-            t.equal(origin, 'mock origin')
-            t.match(message, { error: 'Could not find an authorization_code' })
-        }
+        postMessage: createPostMessage(t, 'mock origin', { error: 'Could not find an authorization_code' })
     }
 
-    const success = handleRedirect()
+    const actual = handleRedirect()
 
-    t.notOk(success)
+    t.notOk(actual)
     t.end()
 })
 
 tap.test('handleRedirect SHOULD post Error message WHEN called with window opener', async t => {
     globalThis.window = { location: { origin: 'mock origin' } }
     globalThis.window.parent = globalThis.window
-
     globalThis.window.opener = {
-        postMessage: (message, origin) => {
-            t.equal(origin, 'mock origin')
-            t.match(message, { error: 'Could not find an authorization_code' })
-        }
+        postMessage: createPostMessage(t, 'mock origin', { error: 'Could not find an authorization_code' })
     }
 
-    const success = handleRedirect()
+    const actual = handleRedirect()
 
-    t.notOk(success)
+    t.notOk(actual)
     t.end()
 })
 
 tap.test('handleRedirect SHOULD post received error WHEN called with parent with error query param', async t => {
     globalThis.window = { location: { origin: 'mock origin', search: '?error=mockError' } }
-
     globalThis.window.parent = {
-        postMessage: (message, origin) => {
-            t.equal(origin, 'mock origin')
-            t.match(message, { error: 'mockError' })
-        }
+        postMessage: createPostMessage(t, 'mock origin', { error: 'mockError' })
     }
 
-    const success = handleRedirect()
+    const actual = handleRedirect()
 
-    t.notOk(success)
+    t.notOk(actual)
     t.end()
 })
 
 tap.test('handleRedirect SHOULD post received error WHEN called with parent with error hash param', async t => {
     globalThis.window = { location: { origin: 'mock origin', search: '', hash: '#error=mockError' } }
-
     globalThis.window.parent = {
-        postMessage: (message, origin) => {
-            t.equal(origin, 'mock origin')
-            t.match(message, { error: 'mockError' })
-        }
+        postMessage: createPostMessage(t, 'mock origin', { error: 'mockError' })
     }
 
-    const success = handleRedirect()
+    const actual = handleRedirect()
 
-    t.notOk(success)
+    t.notOk(actual)
     t.end()
 })
 
 tap.test('handleRedirect SHOULD post received code WHEN called with parent with code query param', async t => {
     globalThis.window = { location: { origin: 'mock origin', search: '?code=mockCode' } }
-
     globalThis.window.parent = {
-        postMessage: (message, origin) => {
-            t.equal(origin, 'mock origin')
-            t.match(message, { authorization_code: 'mockCode' })
-        }
+        postMessage: createPostMessage(t, 'mock origin', { authorization_code: 'mockCode' })
     }
 
-    const success = handleRedirect()
+    const actual = handleRedirect()
 
-    t.ok(success)
+    t.ok(actual)
     t.end()
 })
 
 tap.test('handleRedirect SHOULD post received code WHEN called with opener with code query param', async t => {
     globalThis.window = { location: { origin: 'mock origin', search: '?code=mockCode' } }
     globalThis.window.parent = globalThis.window
-
     globalThis.window.opener = {
-        postMessage: (message, origin) => {
-            t.equal(origin, 'mock origin')
-            t.match(message, { authorization_code: 'mockCode' })
-        }
+        postMessage: createPostMessage(t, 'mock origin', { authorization_code: 'mockCode' })
     }
 
-    const success = handleRedirect()
+    const actual = handleRedirect()
 
-    t.ok(success)
+    t.ok(actual)
     t.end()
 })
 
 tap.test('handleRedirect SHOULD post received code WHEN called with parent with code hash param', async t => {
     globalThis.window = { location: { origin: 'mock origin', search: '', hash: '#code=mockCode' } }
-
     globalThis.window.parent = {
-        postMessage: (message, origin) => {
-            t.equal(origin, 'mock origin')
-            t.match(message, { authorization_code: 'mockCode' })
-        }
+        postMessage: createPostMessage(t, 'mock origin', { authorization_code: 'mockCode' })
     }
 
-    const success = handleRedirect()
+    const actual = handleRedirect()
 
-    t.ok(success)
+    t.ok(actual)
     t.end()
 })
+
+function createPostMessage(t, expectedOrigin, expectedMessage) {
+    return function postMessage(message, origin) {
+        t.equal(origin, expectedOrigin)
+        t.match(message, expectedMessage)
+    }
+}
